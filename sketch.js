e44d5c4edac6488cee2ad22490f6cb9a9049519e
RYAN SINGH <71920588+ITZ-RYAN111@users.noreply.github.com>
2020-11-13 20:02:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var bananaImage,obstacleImage,obstaclegroup,background,score
+
+
+
+function preload(){
+ backImage=loadImage("jungle.jpg");
+ player_running=
+  loadImage("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");  
+  
+  
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+ // background=createSprite();
+  //background.loadImage("jungle.jpg")
+  //background.velocityX=1;
+}
+
+function draw() {
+
+
+  background(220);
+ // drawsprites();
+}
